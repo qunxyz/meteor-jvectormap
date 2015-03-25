4c4f094284bdc13db74d9fc2741c87a570270b7e
Jacky Qiu <qunxyz@gmail.com>
2015-03-26 05:33:20 +0800

    updated file's url

--- generate.js
+++ generate.js
@@ -3,19 +3,18 @@ var url = require("url");
 
 var output = [];
 
-var jsPrefix = "http://jvectormap.com/js/";
-var cssPrefix = "http://jvectormap.com/css/";
+var urlPrefix = "https://raw.githubusercontent.com/qunxyz/meteor-jvectormap/master/";
 
 // Base file
 output.push({
     file: "jquery-jvectormap-2.0.2.min.js",
-    url: url.resolve(jsPrefix, "jquery-jvectormap-2.0.2.min.js")
+    url: url.resolve(urlPrefix, "jquery-jvectormap-2.0.2.min.js")
 });
 
 // CSS file
 output.push({
     file: "jquery-jvectormap-2.0.2.css",
-    url: url.resolve(cssPrefix, "jquery-jvectormap-2.0.2.css")
+    url: url.resolve(urlPrefix, "jquery-jvectormap-2.0.2.css")
 });
 
 console.log(JSON.stringify(output, null, "    "));
